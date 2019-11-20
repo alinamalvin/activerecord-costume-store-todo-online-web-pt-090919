c20cb5811d78073ac7359edd8f455b39fc239c83
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-20 04:30:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,13 @@
-class CreateCostumes < ActiveRecord::Migration[5.1]
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.string :image_url
-      t.string :size 
-      t.timestamps
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+    def change 
+        create_table :costume_stores do |t|
+            t.text :name
+            t.text :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees 
+            t.boolean :still_in_business
+            t.datetime :opening_time
+            t.datetime :closing_time
+        end
     end
-  end
 end
